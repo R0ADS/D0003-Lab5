fdc5a2a3fdc3f5f71f0a6a78e1bebcf8e9a892c5
Road <antale-3@student.ltu.se>
2025-03-24 18:46:35 +0100

    stink

--- D0003E-Lab_5/main.c
+++ D0003E-Lab_5/main.c
@@ -6,13 +6,41 @@
  */ 
 
 #include <avr/io.h>
+#define FOSC 8000000
+#define BAUD 9600
+#define MYUBRR ((FOSC/(16*BAUD))-1)
 
+void init(void){
+	/* Set baud rate */
+	UBRR0H = (uint8_t)(MYUBRR<<8);
+	UBRR0L = (uint8_t)(MYUBRR);
+	/* Enable receiver and transmitter */
+	UCSR0B = (1<<TXEN0) | (1<<RXEN0) | (1<<RXCIE0);
+
+}
+
+uint8_t USART_Receiver(void){
+	
+	/* Wait for data to be received */
+	while ( !(UCSR0A & (1<<RXC0)) );
+	/* Return said data from buffer */
+	return UDR0;
+}
+
+uint8_t USART_Transmit(uint8_t data){
+	while ( !(UCSR0A & (1<<UDRE0)) );
+	UDR0 = data;
+}
 
 int main(void)
 {
-    /* Replace with your application code */
-    while (1) 
+	int dicks = 1;
+	init();
+
+    while (dicks) 
     {
+		uint8_t temptational = USART_Receiver();
+		USART_Transmit(temptational);
     }
 }
 
