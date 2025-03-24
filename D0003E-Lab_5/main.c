60b1b9b5dcba18c029a11441c16b7cad56ca0989
Road <antale-3@student.ltu.se>
2025-03-24 16:47:55 +0100

    first commit

---
+++ D0003E-Lab_5/main.c
@@ -0,0 +1,18 @@
+/*
+ * D0003E-Lab_5.c
+ *
+ * Created: 2025-03-24 16:43:50
+ * Author : anton
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
